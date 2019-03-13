7f6016fd97b68015ea5ffb822da5445acf7453dd
frankievalentine <frankievalentine9@gmail.com>
2019-03-12 20:01:29 -0700

    first commit & site initiated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `V3 Digital Studio`,
+    description: `A design & development studio based in the San Francisco Bay Area`,
+    author: `@frankievalentine`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,6 +13,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/images/v3favicon.png",
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -21,12 +27,13 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000`,
+        theme_color: `#000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/v3favicon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,86 @@
 import React from "react"
-import { Link } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
 
-import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import "../styles/normalize.css"
+import "../styles/main.css"
+
+const Main = styled.main`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const SectionH2 = styled.h2`
+  width: 300px;
+  font-size: 18px;
+  margin-left: -30px;
+  padding-left: 150px;
+  font-weight: 300;
+  text-align: left;
+  line-height: 30px;
+
+  @media screen and (max-width: 445px) {
+    width: 200px;
+  }
+`
+
+const SectionAddress = styled.address`
+  font-family: Tinos, sans-serif;
+  font-size: 14px;
+  font-style: italic;
+  margin-left: -30px;
+  padding-left: 150px;
+  line-height: 30px;
+`
+
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      {
+        site {
+          siteMetadata {
+            description
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <SEO
+          title="Home"
+          keywords={[
+            `v3 digital studio`,
+            `agency`,
+            `web development`,
+            `web design`,
+          ]}
+        />
+        <Main>
+          <section>
+            <div style={{ maxWidth: `120px`, marginBottom: `1.45rem` }}>
+              <Image />
+            </div>
+            <hr />
+            <SectionH2>{data.site.siteMetadata.description}</SectionH2>
+            <SectionAddress>
+              <a href="mailto:info@v3digital.studio">info@v3digital.studio</a>
+              <br />
+              925.660.5981
+            </SectionAddress>
+          </section>
+        </Main>
+      </>
+    )}
+  />
 )
 
 export default IndexPage
